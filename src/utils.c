86638a8e7ed85f56d1240a8dbe17a30b1466a647
aeis <aiis7905@hanmail.net>
2017-10-07 01:27:40 +0900

    utills.c change2

--- src/utils.c
+++ src/utils.c
@@ -1,24 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "utils.h"
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
+{
 
-	char *token = NULL;
-	char *arg[100];
-	char *s = " \t\n";
+	char* arg[100];
+	char* s = " \t\n";
 
-	char str[50];
+	char str[100];
+
 	strcpy(str,command);
 	str[strlen(str)] = 0;
  	arg[0] = strtok(str, s);
 
-	*argc = 1;
 
+	(*argc) = 1;
+	if(arg[0]){
 	while(arg[*argc] = strtok(NULL,s))
 	{
 		(*argc)++;
 	}
-	
+
 	(*argv) = (char**)malloc(sizeof(char*) * (*argc));
 
 	for(int i = 0 ; i<(*argc) ; i++)
@@ -26,11 +32,11 @@ void mysh_parse_command(const char* command,
 		(*argv)[i] = (char*)malloc(sizeof(char)*strlen(arg[i])+1);
 	 	strcpy((*argv)[i],arg[i]);
   	}
-
-	for(int i = 0; i<(*argc); i++)
-	{
-		free((*argv)[i]);
-		free((*argv));
+	
 	}
-
+	else{
+		(*argv) = (char**)malloc(sizeof(char*));
+		(*argv)[0] = (char*)malloc(sizeof(char*)+1);
+		strcpy((*argv)[0],"");
+	}
 }
