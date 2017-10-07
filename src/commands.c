15a90ad7849f50bf7200843717c0f9a30bde941e
aeis <aiis7905@hanmail.net>
2017-10-07 23:09:40 +0900

    commands.c change

--- src/commands.c
+++ src/commands.c
@@ -1,4 +1,7 @@
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 
 #include "commands.h"
 
@@ -7,7 +10,10 @@ int do_cd(int argc, char** argv) {
     return -1;
 
   // TODO: Fill it!
-  return 0;
+	
+//	validate_cd_argv(argc,argv);
+
+    return 0;
 }
 
 int do_pwd(int argc, char** argv) {
@@ -16,15 +22,37 @@ int do_pwd(int argc, char** argv) {
 
   // TODO: Fill it!
 
+//	validate_pwd_argv(argc,argv);
+
   return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
   // TODO: Fill it!
+	
+	if(strcmp(argv[0],"cd") == 0 && argc == 2)
+	{	
+		if(strcmp(argv[1],".") == 0)
+		{
+			return 1;
+		}
+		chdir(argv[1]);
+	}
+	else 
+		return 0;
   return 1;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
   // TODO: Fill it!
-  return 1;
+	if((strcmp(argv[0],"pwd") == 0)&& argc == 1){
+		char str[1024];
+		getcwd(str, sizeof(str));
+		printf("%s\n", str);
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
 }
